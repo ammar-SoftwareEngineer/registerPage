d6fe767e19613659c447d75ad9b7e46c14fe9757
ammar yasser <aboamer0548@gmail.com>
2024-05-16 11:59:13 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Login from "./Component/Login/Login";
 import Register from "./Component/Register/Register";
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Router>
         <Routes>
- 
-          <Route path="/login" exact Component={Login} />
+          <Route path="/login" Component={Login} />
           <Route path="/" exact Component={Login} />
-          <Route path="/register" exact Component={Register} />
-     
+          <Route path="/register" Component={Register} />
         </Routes>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/registerPage">
+   
       
       <App />
-    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
