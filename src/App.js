c1a6081112763ad36761f14a03f16d75be84d9f2
ammar yasser <aboamer0548@gmail.com>
2024-05-16 11:23:42 +0300

    update

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Login from "./Component/Login/Login";
 import Register from "./Component/Register/Register";
-import { Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <Router basename="/registerPage">
       <Routes>
         <Route path="/login" Component={Login} />
-        <Route path="/registerPage/" Component={Login} />
+        <Route path="/" Component={Login} />
         <Route path="/register" Component={Register} />
         
       </Routes>
-    </div>
+      </Router>
   );
 }
 
