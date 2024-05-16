7e88095dedb536517445469c9884910ce6e4139f
ammar yasser <aboamer0548@gmail.com>
2024-05-16 19:45:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import Login from "./Component/Login/Login";
 import Register from "./Component/Register/Register";
 
-import { HashRouter , Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+  
         <Routes>
           <Route path="/" Component={Login} />
           <Route path="/login" Component={Login} />
           <Route path="/register" Component={Register} />
         </Routes>
-      </HashRouter>
+
     </div>
   );
 }
